879ac17d260026f3fc0117ecf11c2c21e70ce80b
ryota <ryota.fgpnhngkm@gmail.com>
2026-06-17 18:19:07 +0900

    シリアルテスト成功、カスタムメッセージ追加

--- src/gateway_pkg/src/gateway.cpp
+++ src/gateway_pkg/src/gateway.cpp
@@ -1,5 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "serial/serial.h"
+#include "std_msgs/msg/float32_multi_array.hpp"
+#include "std_msgs/msg/float32.hpp"
+
 
 
 class gateway_node : public rclcpp::Node{
@@ -10,7 +13,11 @@ class gateway_node : public rclcpp::Node{
 
 
 			timer = this->create_wall_timer(std::chrono::milliseconds(10),std::bind(&gateway_node::timer_callback,this));
-
+			
+			auto qos = rclcpp::QoS(rclcpp::KeepLast(10));
+			sub_angle_data = this->create_subscription<std_msgs::msg::Float32MultiArray>("angle_data",qos,std::bind(&gateway_node::angles_callback,this,std::placeholders::_1));
+			
+			sub_rpm_data = this->create_subscription<std_msgs::msg::Float32>("rpm_data",qos,std::bind(&gateway_node::rpm_callback,this,std::placeholders::_1));
 			
 
 
@@ -20,14 +27,51 @@ class gateway_node : public rclcpp::Node{
 
 		rclcpp::TimerBase::SharedPtr timer;
 		serial::Serial& my_serial;
+		rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_angle_data;
+		rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr sub_rpm_data;
+
+		float angles[6];
+		float rpm;
+
+		void rpm_callback(const std_msgs::msg::Float32::SharedPtr msg){
+			rpm = msg->data;
+		}
+
+
+		void angles_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg){
+			for (int i=0;i<6;i++){
+				angles[i] = msg->data[i];
+			}
+		}
 
 		void timer_callback(){
 
-			float data[8] = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8};
+			angles[0] = 0.0;
+			angles[1] = 20;
+			angles[2] = 40;
+			angles[3] = 60;
+			angles[4] = 80;
+			angles[5] = 100;
+			
 
-			my_serial.write(reinterpret_cast<uint8_t*>(data),sizeof(data));
 
+			std::string data[7];
+
+			for (int i = 0 ; i < 6 ; i++){
+				data[i]  = "S" + std::to_string(i) + " " + std::to_string(angles[i]);
+			}
+
+			data[6] = "D" + std::to_string(rpm);
+
+
+
+			//float data[8] = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8};
+			for (int i = 0 ; i < 7 ; i++){
+				my_serial.write(data[i]);
+			}
 		}
+
+
 };
 
 int main(int argc, char **argv){
